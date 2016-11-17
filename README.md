ca1beab9c68159814d70957063df716d6d2bba61
Guillaume R <guillaume.raille@gmail.com>
2016-11-17 14:10:55 +0100

    Initial Commit
    Initial implementation with angularfire and boostrap

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # CardGeneratorWeb
+
+Connecté en temps réel à la base de donnée Firebase avec angularFire ( Angularjs + Firebase).
+
+Pour le lancer en local il faut installer npm puis suivre les directives pour installer Firebase CLI.
+
+Et la commande:
+
+firebase serve
+
+lancera un serveur en local.
+
+Pour deployer les modifications locales:
+
+firebase deploy
---
+++ public/app.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBUqtGx5JnZnLVclM-kRVhi-06jb1uBnOE",
+  authDomain: "epflsurvival-abd33.firebaseapp.com",
+  databaseURL: "https://epflsurvival-abd33.firebaseio.com",
+  storageBucket: "epflsurvival-abd33.appspot.com",
+  messagingSenderId: "166394322782"
+};
+firebase.initializeApp(config);
+
+var app = angular.module("myApp", ["firebase"]);
+
+app.controller("Ctrl", function($scope, $firebaseArray) {
+  var ref = firebase.database().ref().child("cards");
+  $scope.cards = $firebaseArray(ref);
+});
